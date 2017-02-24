1b49f9c51f009ceb30826e80a5d9a2c8cede08c5
Helen Miller <helen.miller@xamarin.com>
2017-02-23 21:34:09 -0500

    Adding tech Gadgets practice app, fixing rendering issue to tax Calculator, updates to CityGuide

---
+++ TechGadget/app/src/main/java/com/example/helenmiller/techgadget/Nest.java
@@ -0,0 +1,13 @@
+package com.example.helenmiller.techgadget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Nest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nest);
+    }
+}
---
+++ TechGadget/app/src/main/java/com/example/helenmiller/techgadget/iPhone.java
@@ -0,0 +1,13 @@
+package com.example.helenmiller.techgadget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class iPhone extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_i_phone);
+    }
+}
