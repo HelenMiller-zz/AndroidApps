806d0e78d7a0ffdc42171316a8a765bca75a9a39
Helen Miller <helen.miller@xamarin.com>
2017-03-28 10:03:22 -0400

    [Boogie] updates to icons

--- CityGuide/app/src/main/java/com/example/helenmiller/cityguide/Willis.java
+++ CityGuide/app/src/main/java/com/example/helenmiller/cityguide/Willis.java
@@ -1,10 +1,13 @@
 package com.example.helenmiller.cityguide;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by helenmiller on 3/28/17.
+ */
 
 public class Willis extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
