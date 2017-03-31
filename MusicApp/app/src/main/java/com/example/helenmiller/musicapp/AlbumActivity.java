80b1600d9724892d90fe851fec97b88450272f34
Helen Miller <helen.miller@xamarin.com>
2017-03-30 20:11:39 -0400

    [Sailing] app complete date picker example

--- MusicApp/app/src/main/java/com/example/helenmiller/musicapp/AlbumActivity.java
+++ MusicApp/app/src/main/java/com/example/helenmiller/musicapp/AlbumActivity.java
@@ -6,6 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AlbumActivity extends ListActivity {
 
     @Override
@@ -13,15 +16,36 @@ public class AlbumActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_album);
 
-       //int albumPosition = getIntent().getIntExtra("groupPosition", -1);
-        //int songPosition = getIntent().getIntExtra("childPosition", -1);
+        int albumPosition = getIntent().getIntExtra("groupPosition", -1);
+        int songPosition = getIntent().getIntExtra("childPosition", -1);
         //String title = getIntent().getStringExtra("Album Title");
+
+        final List<String> songsShakedown = new ArrayList<String>();
+        songsShakedown.add("Good Lovin");
+        songsShakedown.add("France");
+        songsShakedown.add("Shakedown Street");
+        songsShakedown.add("Operator");
+        songsShakedown.add("Serengetti");
+        songsShakedown.add("Fire on the Mountain");
+        songsShakedown.add("I Need a Miracle");
+        songsShakedown.add("From the Heart of Me");
+        songsShakedown.add("Stagger Lee");
+        songsShakedown.add("All New Minglewood Blues");
+        songsShakedown.add("If I Had the World to Give");
+
+
 
         //list of Albums
         String[] albums =
                 {
                         "Dookie", "American Idiot", "Let it Be", "Revolver", "American Beauty", "Shakedown Street", "Rift", "Junta"
                 };
+
+
+
+
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.activity_album, R.id.listSongs, songsShakedown));
+
  /*
         //lists of Album Songs
         String[] songsDookie =
@@ -34,17 +58,11 @@ public class AlbumActivity extends ListActivity {
                         "Two of Us", "Dig a Pony", "Whatsername", "Across the Universe", "I Me Mine", "Dig It", "Let it Be", "Maggie Mae", "I've Got a Feeling", "One After 909", "The Long and Winding Road", "For You Blue", "Get Back"
                 };
 
-        String[] songsShakedownStreet =
-                {
-                        "Good Lovin", "France", "Shakedown Street", "Operator", "Serengetti", "Fire on the Mountain", "I Need a Miracle", "From the Heart of Me", "Stagger Lee", "All New Minglewood Blues", "If I Had the World to Give"
-                };
+
         String[] songsRift =
                 {
                         "Rift", "Fast Enough For You", "Maze", "Sparkle", "Horn", "The Wedge", "My Friend, My Friend", "Weigh", "All Things Reconsidered", "Mound", "It's Ice", "The Horse", "Silent in the Morning"
                 };
-
-        setListAdapter(new ArrayAdapter<String>(this, R.layout.activity_album, R.id.listSongs, songsShakedownStreet ));
-
     }*/
 
     }
